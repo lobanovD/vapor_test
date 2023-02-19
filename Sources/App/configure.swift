f804f8afbc0eef21d33b0522c6ffa7b44a416e9f
Dmitrii Lobanov <dvmald@icloud.com>
2023-02-19 21:09:50 +0700

    add start config

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,17 @@
 import Vapor
 
+let isLocal = false
+
 // configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    // register routes
+    
+    if isLocal {
+        app.http.server.configuration.hostname = "127.0.0.1"
+        app.http.server.configuration.port = 8080
+    } else {
+        app.http.server.configuration.hostname = "0.0.0.0"
+        app.http.server.configuration.port = 80
+    }
+    
     try routes(app)
 }
